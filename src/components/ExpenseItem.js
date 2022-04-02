4729a37a84f77dc91511c24da11cfcbe31094ced
zermiftah <muhammadmiftahudin113@gmail.com>
2022-04-03 04:55:30 +0630

    Writing a more complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div>
+            <div>March 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.76</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
