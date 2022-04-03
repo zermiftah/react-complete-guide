66bd814ee604a3a025a7b709a67f0efe0e1453b7
zermiftah <muhammadmiftahudin113@gmail.com>
2022-04-03 13:21:49 +0630

    A closer look at JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from './components/Expenses'
 
 function App() {
@@ -28,12 +29,17 @@ function App() {
     },
   ];
 
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
-    </div >
+  return React.createElement(
+    'div', {},
+    React.createElement('h2', {}, "Let\'s get started"),
+    React.createElement(Expenses, { items: expenses })
   );
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses} />
+  //   </div >
+  // );
 }
 
 export default App;
